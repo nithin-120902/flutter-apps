519cb091f6b8ab3d171f76ed8a93db0ddf10a6ed
nithin1209-bit <420251@student.nitandhra.ac.in>
2023-05-24 13:18:01 +0530

    updated code'

--- bmi/lib/calculate.dart
+++ bmi/lib/calculate.dart
@@ -1,27 +1,22 @@
+import 'package:bmi/response.dart';
+
 class Calculate{
-  double? height;
-  double? weight;
-  double? _bmi;
-  String? message;
+  
+  static Response bmi(height,weight){
+    double bmi;
+    String message='';
+    height=height/100;
+    bmi = (weight / (height * height));
 
-  Calculate({this.height,this.weight,this.message=""});
-
-  List<String> bmi(){
-    if (height == null || height! <= 0 || weight == null || weight! <= 0) {
-      return ["No result","Enter Valid Height and Weight"];
-    }
-    height=height!/100;
-    _bmi = (weight! / (height! * height!));
-
-    if (_bmi! < 18.5) {
+    if (bmi < 18.5) {
       message = "You are in Under weight category.";
-    } else if (_bmi! < 25) {
+    } else if (bmi < 25) {
       message = "You are in Noraml weight category.";
-    } else if (_bmi! < 30) {
+    } else if (bmi < 30) {
       message = "You are in Over weight category.";
-    } else if (_bmi! > 30) {
+    } else if (bmi > 30) {
       message = "You are Obese";
     }
-    return [_bmi.toString(),message.toString()];
+    return Response(bmi: bmi, message: message);
   }
 }
--- bmi/lib/main.dart
+++ bmi/lib/main.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
 
 import 'package:bmi/calculate.dart';
+import 'package:bmi/request.dart';
+import 'package:bmi/response.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +17,18 @@ class BMI extends StatefulWidget {
 }
 
 class _BMIState extends State<BMI> {
-
-  List<String> _bmi = ["No Result","Enter height and weight"];
   
   final _heightController = TextEditingController();
 
   final _weightController = TextEditingController();
 
-  void calculate(){
-    final double? height = double.tryParse(_heightController.value.text);
-    final double? weight = double.tryParse(_weightController.value.text);
+  Response response = Response(bmi: 0, message: "Please Enter the values");
 
-    Calculate cal = Calculate(height: height,weight: weight);
-
-    setState(() {
-      _bmi = cal.bmi();
-    });
-  }
+  final _key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal[900],
@@ -44,68 +38,92 @@ class _BMIState extends State<BMI> {
           backgroundColor: Colors.teal[300],
         ),
       body: Center(
-        child: SizedBox(
+        child: Container(
           width:320,
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                    Radius.circular(10.0)
+          padding:EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                  Radius.circular(10.0)
+              ),
+              color: Colors.white,
+            ),
+          child: Form(
+            key: _key,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Weight in kgs',
+                    labelStyle: TextStyle(
+                      fontSize: 20
+                    ),
+                    floatingLabelAlignment: FloatingLabelAlignment.start
+                  ),
+                  validator: (value){
+                    return (value == null || value.isEmpty) ? 'Numeric Value Required' : null;
+                  },
+                  cursorHeight: 25.0,
+                  keyboardType: const TextInputType.numberWithOptions(decimal:true),
+                  controller: _weightController,
                 ),
-                color: Colors.white,
-              ),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.numberWithOptions(
-                        decimal: true),
-                    decoration:
-                        InputDecoration(labelText: 'Weight (Kg)',icon: Icon(Icons.scale_outlined)),
-                    controller: _weightController,
-                  ),
-                  SizedBox(
-                    height:20,
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.numberWithOptions(
-                        decimal: true),
-                    decoration:
-                         InputDecoration(labelText: 'Height (cm)',icon: Icon(Icons.height)),
-                    controller: _heightController,
-                  ),
-                  SizedBox(
-                    height:20,
-                  ),
-                  ElevatedButton(
-                    onPressed: (){
-                      calculate();
-                    }, 
-                    child:
-                    Text(
-                      "Calculate",
-                    ), 
-                  ),
-                  SizedBox(
+                SizedBox(
+                  height:20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Height in cms',
+                      labelStyle: TextStyle(fontSize: 20),
+                      floatingLabelAlignment: FloatingLabelAlignment.start),
+                  validator: (value) {
+                    return (value == null || value.isEmpty)
+                        ? 'Numeric Value Required'
+                        : null;
+                  },
+                  cursorHeight: 25.0,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: _heightController,
+                ),
+                SizedBox(
+                  height:20,
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      if (_key.currentState!.validate()) {
+                        Request req = Request(
+                            height: double.parse(_heightController.value.text),
+                            weight: double.parse(_weightController.value.text));
+                        response = Calculate.bmi(req.height, req.weight);
+                        print(response.bmi);
+                        print(response.message);
+                      }
+                    });
+                  }, 
+                  child:
+                  Text(
+                    "Calculate",
+                  ), 
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  (response.bmi).toString(),
+                ),
+                SizedBox(
                     height: 30,
-                  ),
-                  Text(
-                    _bmi[0]
-                  ),
-                  SizedBox(
-                      height: 30,
-                  ),
-                  Text(
-                    _bmi[1],
-                  )
-                ],
-              ),
+                ),
+                Text(
+                  (response.message).toString()
+                ),
+              ]
             ),
           ),
         ),
-      ), 
       ),
-    );
+    ),
+  );
   }
 }
